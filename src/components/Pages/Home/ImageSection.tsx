2b27141f46de096b43e80f5f35746c946b5ff792
Aminulhoque01 <aminuhoque53@gmail.com>
2025-08-06 22:21:43 +0600

    image section modified

--- src/components/Pages/Home/ImageSection.tsx
+++ src/components/Pages/Home/ImageSection.tsx
@@ -44,7 +44,7 @@ const ImageSection = () => {
         <div className="image-section bg-[#F8EDE3] pt-6 min-h-screen">
             <div className="text-center pt-8">
                 <div
-                    className={`container mx-auto px-4 py-8 transition-none ${
+                    className={`container mx-auto px-4 py-8 transition-none mt-20 ${
                         isGrid
                             ? 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6'
                             : 'relative h-[400px] sm:h-[500px] md:h-[600px]'
